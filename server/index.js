2a2e5b6a402ec5615eb38b906edb56795bbe2865
Amirali Karimi <itsral.karimi@gmail.com>
2022-12-07 23:14:02 +0330

    tried to connect to the mongoose db but it did not connected

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 
-import clientRoutes from "./routes/client.js"
-import generalRoutes from "/routes/general.js"
-import managementRoutes from "./routes/management.js"
-import salesRoutes from "./routes/sales.js"
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 dotenv.config();
 const app = express();
@@ -21,9 +21,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // ROUTES
-app.use("/client", clientRoutes)
-app.use("/general", generalRoutes)
-app.use("/management", managementRoutes)
-app.use("/sales", salesRoutes)
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
 
-
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.PORT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`server port: ${PORT}`));
+  })
+  .catch((error) => console.log(error + "did not connect"));
